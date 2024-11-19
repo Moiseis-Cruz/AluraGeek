27f8a3f6de82b7d274601355423fdd0112bcab3e
Moiseis Cruz <moizzito@gmail.com>
2024-11-18 21:46:47 -0300

    I created the file to generate the card on the screen from mok

---
+++ src/scripts/mostrarCard.js
@@ -0,0 +1,26 @@
+import { serviceApi } from "./serviceApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(image, name, price){
+    const card = document.createElement("li");
+    card.innerHTML = `  <li>
+                            <div class="product-card">
+                                <img class="card-image" src="${image}" alt="">
+                                <h3 class="product-name">${name}</h3>
+                                <div>
+                                    <p class="product-price">$ ${price}</p>
+                                    <button class="delete-product-button">🗑️</button>
+                                </div>
+                            </div>
+                        </li>`
+
+    return card;
+    };
+
+async function gerarCards() {
+    const listaApi = await serviceApi.listaCards();
+    listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.image, elemento.name, elemento.price)))
+}
+
+gerarCards()
