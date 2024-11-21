e2e04b7860196fa349bf661022a30a0831dd76b5
Moiseis Cruz <moizzito@gmail.com>
2024-11-21 20:56:57 -0300

    I connected the request to the function to create a new card.

--- src/scripts/adicionarCard.js
+++ src/scripts/adicionarCard.js
@@ -1,11 +1,15 @@
+import { serviceApi } from "./serviceApi.js";
+
 const formCards = document.querySelector("[data-form]");
 
-function criarVideo(evento){
+async function criarVideo(evento){
     evento.preventDefault();
 
     const name = document.getElementById("name").value;
     const image = document.getElementById("image").value;
     const price = document.getElementById("price").value;
+
+    await serviceApi.adicionarCard(image, name, price);
 };
 
 formCards.addEventListener("submit", evento => criarVideo(evento));
