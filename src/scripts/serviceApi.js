4ae9d6150ae727e13784c6912540947630a6f254
Moiseis Cruz <moizzito@gmail.com>
2024-11-21 20:47:41 -0300

    I created the asynchronous function to make the "POST" method request.

--- src/scripts/serviceApi.js
+++ src/scripts/serviceApi.js
@@ -4,6 +4,23 @@ async function listaCards() {
     return datas;
 };
 
+async function adicionarCard(image, name, price){
+    const response = await fetch(`http://localhost:3000/card`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            image: image,
+            name: name,
+            price: price
+        })
+    });
+    const datas = await response.json();
+    return datas
+}
+
 export const serviceApi = {
-    listaCards
+    listaCards,
+    adicionarCard
 };
