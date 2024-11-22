166e65351889bfc3ec6592f976b1985ab864950b
Moiseis Cruz <moizzito@gmail.com>
2024-11-22 18:06:53 -0300

    Removed unnecessary code.

--- src/scripts/adicionarCard.js
+++ src/scripts/adicionarCard.js
@@ -3,13 +3,13 @@ import { serviceApi } from "./serviceApi.js";
 const formCards = document.querySelector("[data-form]");
 
 async function criarVideo(evento){
-    evento.preventDefault();
-
     const name = document.getElementById("name").value;
     const image = document.getElementById("image").value;
     const price = document.getElementById("price").value;
 
     await serviceApi.adicionarCard(image, name, price);
+
+    evento.preventDefault();
 };
 
 formCards.addEventListener("submit", evento => criarVideo(evento));
