4e15f9271e5a3f2a7c2f14ff2f84061addbf2461
Moiseis Cruz <moizzito@gmail.com>
2024-11-24 11:51:34 -0300

    I created the event to delete cards and message in case the list is empty.

--- src/scripts/mostrarCard.js
+++ src/scripts/mostrarCard.js
@@ -2,7 +2,7 @@ import { serviceApi } from "./serviceApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(image, name, price){
+function constroiCard(id, image, name, price){
     const card = document.createElement("li");
     card.innerHTML = `  
         <li class="product-card">
@@ -10,16 +10,32 @@ function constroiCard(image, name, price){
             <h3 class="product-name">${name}</h3>
             <div>
                 <p class="product-price">$ ${price}</p>
-                <button class="delete-product-button">🗑️</button>
+                <button class="delete-product-button" data-id="${id}">🗑️</button>
             </div>
-        </li>`
+        </li>`;
 
+        const deleteButton = card.querySelector(".delete-product-button");
+    deleteButton.addEventListener("click", async () => {
+        await serviceApi.deleteCard(id);
+        card.remove();
+    });
     return card;
 };
 
 async function gerarCards() {
     const listaApi = await serviceApi.listaCards();
-    listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.image, elemento.name, elemento.price)));
+
+    lista.innerHTML = ''; 
+
+    if (listaApi.length === 0) {
+        const mensagemVazia = document.createElement("h1");
+        mensagemVazia.textContent = "A lista está vazia";
+        lista.appendChild(mensagemVazia);
+    } else {
+        listaApi.forEach(elemento => {
+            lista.appendChild(constroiCard(elemento.id, elemento.image, elemento.name, elemento.price));
+        });
+    };
 };
 
 gerarCards();
