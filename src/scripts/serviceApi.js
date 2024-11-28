234d23b4c178164fff976f2d753377da26361f41
Moiseis Cruz <moizzito@gmail.com>
2024-11-28 12:40:30 -0300

    I added trycatch to the async functions.

--- src/scripts/serviceApi.js
+++ src/scripts/serviceApi.js
@@ -1,34 +1,46 @@
 async function listaCards() {
-    const response = await fetch(`http://localhost:3000/card`);
-    const datas = await response.json();
-    return datas;
+    try {
+        const response = await fetch(`http://localhost:3000/card`);
+        const datas = await response.json();
+        return datas;
+    } catch (err) {
+        console.log(`Erro ao carregar a lista de produtos: ${err.message}`);
+    };
 };
 
 async function adicionarCard(image, name, price){
-    const response = await fetch(`http://localhost:3000/card`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({
-            image: image,
-            name: name,
-            price: price
-        })
-    });
-    const datas = await response.json();
-    return datas;
+    try {
+        const response = await fetch(`http://localhost:3000/card`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                image: image,
+                name: name,
+                price: price
+            })
+        });
+        const datas = await response.json();
+        return datas;
+    } catch (err) {
+        console.log(`Erro ao tentar solicitar POST: ${err.message}`);
+    };
 };
 
 async function deleteCard(id){
-    const response = await fetch(`http://localhost:3000/card/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json"
-        }
-    });
-    const data = await response.json();
-    return data;
+    try {
+        const response = await fetch(`http://localhost:3000/card/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.log(`Erro ao tentar solicitar DELETE: ${err.message}`);
+    };
 };
 
 export const serviceApi = {
