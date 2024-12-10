27380d6ec071ac77d271da7c984d120b5457085f
Moiseis Cruz <moizzito@gmail.com>
2024-12-10 20:56:48 -0300

    I created the try catch for asynchronous functions.

--- src/scripts/serviceApi.js
+++ src/scripts/serviceApi.js
@@ -1,42 +1,61 @@
 async function cardsList() {
-    const response = await fetch(`http://localhost:3000/card`);
-    
-    if (!response.ok) {
-        throw new Error('Erro ao buscar os cards');
-    }
+    try {
+        const response = await fetch(`http://localhost:3000/card`);
 
-    return response.json();
-}
+        const data = await response.json()
+
+        if (data.erro) {
+            throw new Error('Erro ao buscar os cards');
+        };
+
+        return data;
+    } catch (err) {
+        console.log(err);
+    };
+};
 
 async function addCard(image, name, price) {
-    const response = await fetch(`http://localhost:3000/card`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({ image, name, price })
-    });
+    try {
+        const response = await fetch(`http://localhost:3000/card`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ image, name, price })
+        });
 
-    if (!response.ok) {
-        throw new Error('Erro ao adicionar o card');
-    }
+        const data = await response.json();
 
-    return response.json();
+        if (data.erro) {
+            throw new Error('Erro ao adicionar o card');
+        };
+
+        return data;
+
+    } catch (err) {
+        console.log(err);
+    };
 };
 
 async function deleteCard(id) {
-    const response = await fetch(`http://localhost:3000/card/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json"
-        }
-    });
+    try {
+        const response = await fetch(`http://localhost:3000/card/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
 
-    if (!response.ok) {
-        throw new Error('Erro ao excluir o card');
-    }
+        const data = await response.json();
 
-    return response.json();
+        if (data.erro) {
+            throw new Error('Erro ao excluir o card');
+        };
+
+        return data;
+    } catch (err) {
+        console.log(err);
+    };
 };
 
 export const serviceApi = {
