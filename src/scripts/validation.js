ed98476943c3db001e9eb6b918a788557717bf7d
Moiseis Cruz <moizzito@gmail.com>
2024-12-10 11:18:13 -0300

    I changed the names of the variables and functions.

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,10 +1,12 @@
-export function validarCampos(nameInput, imageInput, priceInput) {
+import { exibirMensagemErro } from "./messages.js";
+
+export function validateFields(nameInput, imageInput, priceInput) {
     const nameValue = nameInput.value.trim();
     const imageValue = imageInput.value.trim();
     const priceValue = priceInput.value.trim();
 
     if (!nameValue || !imageValue || !priceValue) {
-        alert("Preencha todos os campos, por favor!");
+        exibirMensagemErro("Preencha todos os campos, por favor!");
         return false;
     }
 
